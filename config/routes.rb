176c37b2a77dcdac9c888b80bb6f069ec265724f
RyoKitatani <ryo0929m@gmail.com>
2021-02-22 23:57:44 +0000

    user_edit機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root to: "homes#top"
   get "home/about" => "homes#show"
   resources :books, only:[:new, :create, :edit, :index, :show, :destroy, :update]
+  resources :users, only:[:show, :edit, :update]
 end
