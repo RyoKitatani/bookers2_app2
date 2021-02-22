a199822a066e378cc01b54b0f60af86099f5b425
RyoKitatani <ryo0929m@gmail.com>
2021-02-22 22:20:17 +0000

    投稿後表示させる修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,10 @@ class BooksController < ApplicationController
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
   end
 
   def index
@@ -15,4 +19,9 @@ class BooksController < ApplicationController
 
   def destroy
   end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :opinion)
+  end 
 end
