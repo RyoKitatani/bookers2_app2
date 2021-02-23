8ae64a669618e64f343c6079c77e400765538016
RyoKitatani <ryo0929m@gmail.com>
2021-02-23 00:26:28 +0000

    User画像追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,12 +11,12 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to user_path
+    redirect_to user_path(@user.id)
   end
   
   private
   def user_params
-    params.require(:user).permit(:name, :introduction)
+    params.require(:user).permit(:name, :introduction, :image)
   end
 
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'da4561319ede6338367cd402c1c74c2520c923d19927d2ef7e3ae3856a7eb595a17bfe6fc6b1b783c34ff9b5357166f09e593258aeebd836a36af5bbaf953097'
